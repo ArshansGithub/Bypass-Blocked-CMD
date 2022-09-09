5075a4066fe6ca0865812a068a6376ca93302315
ArshansGithub <111618520+ArshansGithub@users.noreply.github.com>
2022-09-09 14:55:54 -0700

    Create bypass.py

---
+++ bypass.py
@@ -0,0 +1,27 @@
+import subprocess
+
+base = ""
+
+command = input("Command: ").split()
+
+def compute(cmd: list, execute: bool):
+        if type(execute) is not bool:
+                raise TypeError(f"Expected boolean in second argument instead of {type(execute)}")
+        elif type(cmd) is not list:
+                raise TypeError(f"Expected list in first argument instead of {type(cmd)}")
+        global base
+        for value in cmd:
+                if "subprocess.call([" not in base:
+                        base += "subprocess.call(["
+                base += f"'{value}',"
+        if execute:
+                return exec(base[:-1] + "])")
+        else:
+                return base[:-1] + "])"
+
+try:
+        compute(cmd=command, execute=True)
+        input()
+except FileNotFoundError:
+    print("Invalid Command")
+    input()
